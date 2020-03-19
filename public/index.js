fac238f51337a0a904be399924a69683de014165
eafelix <efelixagustin@gmail.com>
2020-03-19 17:20:48 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,43 +1,40 @@
-$(document).ready(function(){
-    $('.btnLista').click(function(){
-        $.ajax({
-          type: 'GET',
-          url: '/getData',
+$(document).ready(function() {
+  $(".btn-list").click(function() {
+    $.ajax({
+      type: "GET",
+      url: "/getData",
 
-          success: function(data){
-            var dataArr = JSON.parse(data);
-            console.log(data);
-            $('.character-list').empty();
-            dataArr.forEach(function(usuario, index){
-              var itemList = $("<li></li>");
-              var infoUsuario = " " + "nombre: " + usuario.nombre + " " +
-              "edad: " +  usuario.edad;
-              var btnData = $('<button>Editar</button>').addClass(usuario.nombre);
+      success: function(data) {
+        var dataArr = JSON.parse(data);
+        console.log(data);
+        $(".character-list").empty();
+        dataArr.forEach(function(user, index) {
+          var itemList = $("<li></li>");
+          var userInfo =
+            " " + "Name: " + user.name + " " + "Age: " + user.age;
+          var btnData = $("<button>Edit</button>").addClass(user.name);
 
-              itemList.text(infoUsuario);
-              itemList.append(btnData);
-              $('.character-list').append(itemList);
-            })
-          }
-
-
+          itemList.text(userInfo);
+          itemList.append(btnData);
+          $(".character-list").append(itemList);
         });
-    });
-
-    $('.btn2').click(function(){
-      var input1 = $('#input1').val();
-        $.ajax({
-          type: 'POST',
-          url: '/postData',
-          data: JSON.stringify({ name: input1, location: "Boston" }),
-          success: function(data){
-
-            console.log("sale un john");
-            console.log(data);
-          },
-          headers: {
-        'Content-Type': 'application/json'
       }
-        });
     });
+  });
+
+  $(".btn2").click(function() {
+    var input1 = $("#input1").val();
+    $.ajax({
+      type: "POST",
+      url: "/postData",
+      data: JSON.stringify({ name: input1, location: "Boston" }),
+      success: function(data) {
+        console.log("User Created");
+        console.log(data);
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  });
 });
