1b6ee2d5d0d7c10b016f9f9aaec3b357e2b7c196
eafelix <efelixagustin@gmail.com>
2020-03-19 17:19:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,40 @@
-var express = require('express'),
-  helmet = require('helmet'),
-  bodyParser = require('body-parser');
+var express = require("express"),
+  helmet = require("helmet"),
+  bodyParser = require("body-parser");
 
 var app = express();
 
-var usuario = [{nombre: 'pepe', edad: 80},
-{nombre: 'pepito', edad: 70},
-{nombre: 'jose', edad: 60}];
+var usuario = [
+  { name: "pepe", age: 80 },
+  { name: "pepito", age: 70 },
+  { name: "jose", age: 60 }
+];
 
 app.use(helmet());
 app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/getData', function (req, res) {
+app.get("/getData", function(req, res) {
   res.send(JSON.stringify(usuario));
-
 });
 
 // json
-app.post('/postData', function (req, res) {
+app.post("/postData", function(req, res) {
   console.log(req.body);
-  res.send('todo ok');
+  res.send(200, "ok");
 });
 
 // form
-app.post('/createUser', function (req, res) {
+app.post("/createUser", function(req, res) {
   console.log(req.body);
-  res.send('todo ok');
+  res.send(200, "ok");
 });
 
-
-
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log("Example app listening at http://%s:%s", host, port);
 });
